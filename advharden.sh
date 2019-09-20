9e5916cee63f84e2ed2503cf95b0c9968420bc63
Luke Blevins <lukeblevins15@gmail.com>
2019-09-19 21:52:16 -0400

    Initial Commit after conversion to Bash Script

---
+++ advharden.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+commence_hardening () {
+    countdown
+    compare_users
+}
+
+countdown () {
+    echo "Program will proceed with Ubuntu hardening in 5000 ms"
+    for((i=5;i>0;i--));
+    do
+        echo "Starting in ${i}000 ms"
+        sleep 1s
+    done
+    echo -e "Hardening operations commencing\n"
+}
+
+compare_users () {
+    # Get array of allowed users from specified file
+    allowedUsers=()
+    n=1
+    while read line
+    do
+        if [ ! -z "$line" ]
+        then
+            allowedUsers[$((${n}-1))]=$line
+            echo "Allowed user $line found"
+        fi
+        n=$((n+1))
+    done < $all_users_path
+    echo -e "\nFound ${#allowedUsers[@]} total permitted users"
+
+    allowedSudoUsers=()
+    n=1
+    while read line
+    do
+        if [ ! -z "$line" ]
+        then
+            allowedSudoUsers[$((${n}-1))]=$line
+            echo "Allowed sudo user $line found"
+        fi
+        n=$((n+1))
+    done < $sudo_users_path
+    echo -e "\nFound ${#allowedSudoUsers[@]} permitted sudo users"
+
+    echo -e "\nComparing lists to local system. Please stand by..."
+    sysUsersArr=()
+    n=1
+    while read line
+    do
+        if [ ! -z "$line" ]
+        then
+            sysUsersArr[$((${n}-1))]=$line
+        fi
+        n=$((n+1))
+    done < <(ls -ld /home/*/ | cut -f3 -d'/')
+    echo -e "\nFound ${#sysUsersArr[@]} total system users"
+    
+    for system_user in "${sysUsersArr[@]}"
+    do
+        if [[ " ${allowedUsers[*]} " != *"$system_user"* ]]
+        then
+            read -p "Remove and purge disallowed user ${system_user} (y/n)? " answer
+            if [ $answer == 'y' ] || [ $answer == 'Y' ]
+            then
+                userdel -r ${system_user}
+            fi
+        fi
+    done
+    
+}
+
+# Path to all users file argument
+all_users_path="$1"
+# Path to sudo users file argument
+sudo_users_path="$2"
+
+if [ "$all_users_path" == "help" ]
+then
+    echo "advharden"
+    echo "A program to rapidly harden an Ubuntu Linux machine"
+    echo "Usage: advharden [ALLUSERSPATH] [SUDOUSERSPATH]"
+    echo ""
+    echo -e "[ALLUSERSPATH]|The path to a user-defined file of all permitted users\n[SUDOUSERSPATH]|The path to a user-defined file of all permitted SUDO users" | column -tc "Option,Meaning" -s '|'
+else
+    echo -e "Welcome to AdvHarden\nCreated by Luke Blevins\nVersion 2019.0.0.2\n"
+    echo "Disclaimer: It is advisable to complete forensics questions first"
+    read -p "Have the forensics questions been completed (y/n)? " answer
+    if [ $answer == 'y' ] || [ $answer == 'Y' ]
+    then
+        commence_hardening
+    fi
+fi
+
